6958ae75e79e65b81aaa051a04b28a86df05229b
selfpln <selfplanltd@gmail.com>
2025-12-24 16:30:26 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+import { drive } from "./drive.js";
+
+app.get("/test-drive", async (req, res) => {
+  try {
+    await drive.files.list({ pageSize: 1 });
+    res.json({ success: true, message: "Google Drive connected" });
+  } catch (err) {
+    res.status(500).json({ success: false, error: err.message });
+  }
+});
+
 import express from "express";
 import dotenv from "dotenv";
 
